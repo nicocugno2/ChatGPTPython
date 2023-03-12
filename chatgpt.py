5477f906f4a874f03c3b3979b81277b0bf46ecdd
nicocugno <nicocugno2@gmail.com>
2023-03-12 20:05:39 -0300

    Chat GPT con Python Para poder sacarle provecho a ChatGPT sin iniciar ir siempre a la pagina de la AI

---
+++ chatgpt.py
@@ -0,0 +1,17 @@
+import openai
+
+
+openai.api_key = "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"
+
+
+while True:
+    prompt = input("\ningrese su preguntra: " )
+
+    if prompt == "exit":
+        break
+
+    answer = openai.Completion.create(engine="text-davinci-003",
+                                                prompt=prompt,
+                                                max_tokens=2048)
+
+    print(answer.choices[0].text)
